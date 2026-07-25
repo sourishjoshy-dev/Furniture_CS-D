bc44830db837ed6e0842eeec41f6b16079252b50
sourish <sourishjoshy@gmail.com>
2026-07-25 22:06:38 +0530

    Edited webpages

--- items/views.py
+++ items/views.py
@@ -66,7 +66,7 @@ def logout_view(request):
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = AuthenticationForm(request, data=request.POST)       
         if form.is_valid():
             user = form.get_user()
             auth_login(request, user)
